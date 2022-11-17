578e7a37bfff5efeca9f7de1d982e62c90a183e3
Пользователь <Ilnur0939>
2022-11-17 20:41:13 +0500

    Change to javascript

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 let email = document.querySelector('#email')
-let submit = document.querySelector('#submit')
 let form = document.querySelector('#form')
-let txt = document.querySelector('#txt')
-let box = document.querySelector('#box')
 let img = document.querySelector('#img')
 
 form.addEventListener('submit' , function(evt){
